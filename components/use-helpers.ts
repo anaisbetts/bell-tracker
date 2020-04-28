145dbe4eeff7977a22f73425368426b449720e69
Anaïs Betts <anais@anaisbetts.org>
2020-04-27 17:02:19 -0700

    Create a Result<T> class and port use-helpers to it

--- components/use-helpers.ts
+++ components/use-helpers.ts
@@ -1,25 +1,36 @@
 import { getValue, Model, when } from '@whenjs/when';
 import { useEffect, useState } from 'react';
-import { Observable } from 'rxjs';
+import { from, Observable } from 'rxjs';
+import { Result } from './result';
 
 export type PropSelector<TIn, TOut> = ((t: TIn) => TOut);
 
 export function useObservable<T>(
   target: () => Observable<T>,
   initial?: T,
-): T | undefined {
-  const [ret, setter] = useState(initial);
+): Result<T | undefined> {
+  const [ret, setter] = useState(Result.Ok(initial));
   const [obs] = useState(target);
 
   useEffect(
     () => {
-      const sub = obs.subscribe(setter);
+      const sub = obs.subscribe(
+        x => setter(Result.Ok(x)),
+        ex => setter(Result.Err(ex)));
+
       return sub.unsubscribe.bind(sub);
     },
     [obs],
   );
 
   return ret;
+}
+
+export function usePromise<T>(
+  target: () => Promise<T>,
+  initial?: T,
+): Result<T | undefined> {
+  return useObservable(() => from(target()), initial);
 }
 
 export function useViewModel<T extends Model>(initial: () => T) {
@@ -38,7 +49,7 @@ export function useViewModel<T extends Model>(initial: () => T) {
 export function useWhen<TSource, TRet>(
   model: TSource,
   prop: PropSelector<TSource, TRet>,
-): TRet | undefined {
+): Result<TRet | undefined> {
   const initial = getValue(model, prop);
   const val = initial ? initial.value! : undefined;
 
