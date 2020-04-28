a73eb3a06ffda6c1bd48030753b267a45870c386
Anaïs Betts <anais@anaisbetts.org>
2020-04-27 17:03:14 -0700

    Update our database code

--- components/database.ts
+++ components/database.ts
@@ -13,20 +13,24 @@ export interface BellEvent {
   value: number;
 }
 
-export async function fetchAndUpdateCurrentPoints() {
-  const existingAggregate = await db.doc('aggregates/bellValues').get();
+export function currentBellsDoc() {
+  return db.doc('aggregates/bellValues');
+}
+
+export async function fetchAndUpdateCurrentBells() {
+  const existingAggregate = await (currentBellsDoc().get());
   const currentBells =  existingAggregate.data() as BellAggregate;
 
   const eventsSince = await db.collection('events').where('createdAt', '>=', currentBells.computedAt).get();
   if (eventsSince.docs.length === 0) {
     console.log('No changes to bells since last aggregate!');
-    return currentBells;
+    return currentBells.value;
   }
 
   const newBells = eventsSince.docs
     .reduce((acc, x) => acc += (x.data() as BellEvent).value, currentBells.value);
 
-  db.doc('aggregates/bellValues').update({
+  currentBellsDoc().update({
     computedAt: new Date(),
     value: newBells,
   }).then(
